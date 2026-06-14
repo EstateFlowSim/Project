8214146606009d835e594ae5749a5e9a9c01b9ce
username056 <didtnsdl0406@gmail.com>
2026-06-14 15:06:32 +0900

    feat: 공통 예외 처리, 응답 포맷 구현 및 BaseTimeEntity 버그 수정

--- backend/src/main/java/com/example/home/global/common/BaseTimeEntity.java
+++ backend/src/main/java/com/example/home/global/common/BaseTimeEntity.java
@@ -3,12 +3,14 @@ package com.example.home.global.common;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter(AccessLevel.PROTECTED)
 @SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseTimeEntity {
 
     private LocalDateTime createdAt;
